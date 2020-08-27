aa8d20760ccfa9976a57a7d8525fbfa80639b235
Cezar <cezar.sousa@sysmap.com.br>
2020-08-27 19:32:05 -0300

    Criado analisador de Pacotes

--- index.js
+++ index.js
@@ -1,109 +1,57 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var httpCrawler = require('http');
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-var respRequisicao; 
-var listResp = [];
-const { MongoClient } = require("mongodb");
-var mongoose = require('mongoose'); 
-var lstTopicos = ["remessa","retorno","código","barra","barras","certificado","boleto", "layout","carteira", "cnab", 
-                  "pdf", "carnê", "impressão", "caixa","itaú","bradesco","santander","votorantim", 
-				  "ajuda","dúvida","erro","problema","banco do brasil","inter"];
-var Schema = mongoose.Schema;   
-var responseSchema = new Schema
-(
-  {		
-	responses:[Object]
-  }
-); 			  
-/***************************  Socket.io     ************************************/
-/*******************************************************************************/
-app.get('/listTopicos', function(req, res)
-{
-    res.send(lstTopicos);
-});
-
-app.get('/', function(req, res)
-{
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/listIssues', function(req, res)
-{   
-   mongoose.connect(process.env.MONGO_URI);			
-   var ResponseModel = mongoose.model('responses', responseSchema);       
-   ResponseModel.findOne({}, function (err, responses)
-   {
-       if (err) return handleError(err);
-		
-	   res.send(responses);	    		
-   });  
-});
-
-io.on('connection', function(socket)
-{
-  socket.on('messageBroadcast', function(etapaAtual)
-  {	
-	   mediadorCrawler(etapaAtual);	       
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:'+ port);
-});
-
-function comunicaAoCliente(msg)
-{
-	io.emit('messageBroadcast', msg);
-} 
-/***************************** Específicas do Crawler *****************************/
-var cheerio = require("cheerio");
-function mediadorCrawler(etapa)
-{
-	console.log("Executando a etapa:"+ etapa); 		
-	makeRequest(etapa);			
-} 
-function makeRequest(indice)
-{ 	
-	const request = require('request');	
-	request('https://github.com/BoletoNet/boletonet/issues?page='+ indice, { json: false }, (err, res, body) => 
-	{		
-		var tipoTopico = "indefinido";  
-		var comparacao = "";
-		$ = cheerio.load(body);				
-		$('a').each(function (index, element) 
-		{			
-			if($(element).attr('id') != undefined)
-			{				
-				for(var i=0;i<lstTopicos.length;i++)
-				{
-					comparacao = $(element).text().toLowerCase();
-					if(comparacao.includes(lstTopicos[i]))
-					{
-					  	tipoTopico = lstTopicos[i];
-						i = lstTopicos.length;
-					}
-				}				
-				respRequisicao = { id : $(element).attr('id'), content: $(element).text() , tipoTopico : tipoTopico};				
-			    listResp.push(respRequisicao);
-				console.log(respRequisicao); 
-			}			
-			tipoTopico = "indefinido";  
-		});						
-		if((indice + 1) === 11)
-		   saveList(); 	
-			
-		comunicaAoCliente('Paginação:' + indice + ' analisada...');					
-	});	
-}
-function saveList()
-{
-	mongoose.connect(process.env.MONGO_URI);			
-	var ResponseModel = mongoose.model('responses', responseSchema);  
-	var responseDB = new ResponseModel({responses : listResp});
-	responseDB.save(function (err)
+	const express = require('express');
+	const path = require('path');
+	const app = express();
+	var http = require('http').Server(app);
+	var httpCrawler = require('http');
+	var io = require('socket.io')(http);
+	var port = process.env.PORT || 3002;
+	var respRequisicao; 
+	var listResp = [];
+	var urlBase = 'https://www.npmjs.com/search?q=';
+	app.use(express.static(path.join(__dirname, 'public')));
+	/***************************  Socket.io     ************************************/
+	/*******************************************************************************/
+	app.get('/', function(req, res)
 	{
-		if (err) return console.log(err);		
+	  res.sendFile(__dirname + '/index.html');
 	});
-} 
+	io.on('connection', function(socket)
+	{
+	  socket.on('messageBroadcast', function(etapaAtual)
+	  {	
+		mediadorCrawler(etapaAtual);	       
+	  });
+	});
+	http.listen(port, function(){
+		console.log('listening on *:'+ port);
+	});
+	function comunicaAoCliente(msg)
+	{
+		io.emit('messageBroadcast', msg);
+	} 
+	/***************************** Específicas do Crawler *****************************/
+	var cheerio = require("cheerio");
+	function mediadorCrawler(lib)
+	{		
+		if(lib === null){ 
+		    comunicaAoCliente('FIM');	
+	    }else{
+			console.log("Analisando a biblioteca:"+ lib); 		
+			makeRequest(lib);
+		}				
+	} 
+	function makeRequest(indice)
+	{ 	
+		const request = require('request');	
+		request(urlBase + indice,{ json: false },(err, res, body) => 
+		{	
+			$ = cheerio.load(body);				
+			$('p').each(function (index, element) 
+			{		
+				var desc = $(element).text(); 	
+				respRequisicao = { content: $(element).text() , tipoTopico : indice};							
+				comunicaAoCliente(respRequisicao);							
+			});			
+			comunicaAoCliente('FIM');
+		});	
+	}
